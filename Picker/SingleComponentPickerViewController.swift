c66c2a6d67d23223e09931722bb5cff1de98e093
Loveleen <loveleenkaur598@yahoo.com>
2019-10-11 22:13:36 -0400

    Picker

---
+++ Picker/SingleComponentPickerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SingleComponentPickerViewController.swift
+//  Picker
+//
+//  Created by Student on 2019-10-09.
+//  Copyright © 2019 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class SingleComponentPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
